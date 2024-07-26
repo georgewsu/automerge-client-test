9b1d893878044d98904755a2cea9833562a28262
George Su <gsu@blend.com>
2024-07-26 13:58:56 -0700

    Update test client

--- src/client.ts
+++ src/client.ts
@@ -4,6 +4,7 @@ import { BrowserWebSocketClientAdapter } from "@automerge/automerge-repo-network
 import assert from "assert";
 
 import { createRequire } from "module";
+
 const require = createRequire(import.meta.url);
 const file = require("./file.json");
 
@@ -18,8 +19,6 @@ const repo1 = new Repo({
 const repo2 = new Repo({
   network: [new BrowserWebSocketClientAdapter(`ws://localhost:${PORT}`)],
 });
-
-console.log(`${new Date().toLocaleString()} created test doc locally`);
 
 const mapProductToAutomerge = (value: any) => {
   if (value === null || typeof value === 'number' || typeof value === 'boolean') {
@@ -41,32 +40,18 @@ const mapProductToAutomerge = (value: any) => {
 const data = mapProductToAutomerge(file);
 const handle1 = repo1.create(data);
 
-handle1.change((doc) => {
-  Object.assign(doc, data);
-});
-
 console.log(`${new Date().toLocaleString()} created test doc in repo`);
-
-// wait to give the server time to sync the document
-// @ts-ignore
-await new Promise((resolve) => setTimeout(resolve, 1000))
-
-console.log(`${new Date().toLocaleString()} waited for 1000ms`);
-
-// withholds existing documents from new peers until they request them
-assert.equal(Object.keys(repo2.handles).length, 0);
 
 console.log(`${new Date().toLocaleString()} calling repo2.find`);
 const handle1found = repo2.find(handle1.url);
-console.log(`${new Date().toLocaleString()} called repo2.find`);
 
 assert.equal(Object.keys(repo2.handles).length, 1);
 
-// @ts-ignore
+console.log(`${new Date().toLocaleString()} waiting for doc to be ready`);
 const docFound = await handle1found.doc(["ready"]);
 
 // @ts-ignore
-const productName = docFound.product.name;
+const productName = docFound?.product?.name;
 console.log(`${new Date().toLocaleString()} doc found with productName value: ${productName}`);
 
 console.log(`Exiting!`);
